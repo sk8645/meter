cc9825a065c0272d0811b60258383c1294a77648
Vitaliy Sklyar <admin@beebit.ru>
2023-07-08 16:03:20 +0300

    add new file and feature

--- meter.c
+++ meter.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-
+#include "feature.h"
 
 int main(int argc, char* argv[])
 {
 
 	printf("Hello!");
 	printf("\nAdd message in feature branch!");
-	printf("\nnew message!");
+	printf("\nnew message! \n");
 
+	func();
 
 	return 0;
 }
